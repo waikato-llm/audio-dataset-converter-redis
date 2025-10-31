c658b6627b83ac38cbc41167f367f74313c825eb
Peter Reutemann <fracpete@gmail.com>
2025-10-31 16:29:49 +1300

    prepared for release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "audio_dataset_converter",
         "kasperl_redis",
     ],
-    version="0.0.2",
+    version="0.1.0",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     entry_points={
